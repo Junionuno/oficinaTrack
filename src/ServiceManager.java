150fb14a787304e16669584971612cc0650dbb2a
Junionuno <junio.geraldo@outlook.com>
2026-06-22 22:02:32 -0300

    Adiciona ServiceManager com assinaturas de métodos essenciais

--- src/ServiceManager.java
+++ src/ServiceManager.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceManager {
+    List<ServiceOrder> orders = new ArrayList<>();
+
+    void registerOrder(ServiceOrder order){
+
+    }
+
+    ServiceOrder searchById(int id){
+
+    }
+
+    void exportCompletedOrders(){
+
+    }
+
+    void SaveData(){
+
+    }
+
+    void loadData(){
+
+    }
+
 
 }
