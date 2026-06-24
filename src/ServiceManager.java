db50e3e9c6248974b974ce9c35cd61c102f2ec43
Geraldo Neto - Suporte TI - Alphaville <geraldoneto@redebatista.edu.br>
2026-06-24 13:08:34 -0300

    Adiciona registerOrder no ServiceManager

--- src/ServiceManager.java
+++ src/ServiceManager.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ServiceManager {
     List<ServiceOrder> orders = new ArrayList<>();
 
     void registerOrder(ServiceOrder order){
+        for (ServiceOrder currentOrder : orders){
+            if (currentOrder.getVehicle().equals(order.getVehicle()) && !currentOrder.isCompleted()){
+                System.out.println("Erro: O veículo " + order.getVehicle().getModel() + "Já possui uma ordem de serviço aberta");
+                return;
+            }
 
+            orders.add(order);
+            System.out.println("Ordem de serviço registrada, veículo: " + order.getVehicle().getModel());
+        }
     }
 
     ServiceOrder searchById(int id){
